3b662bdc461bef80074b80ed93f1c4abae6ffcdb
neuronarchitecture27 <troisiemeoeilagency@gmail.com>
2024-01-30 21:22:21 +0100

    changing theme

--- components/MainLayout.tsx
+++ components/MainLayout.tsx
@@ -48,56 +48,67 @@ export default async function MainLayout({children}: MainLayoutProps) {
    return redirect("/login")
   }
       return (
-          <main className="flex bg-black h-screen ">
+        <main className="flex h-screen bg-black">
         <SideNavigation />
-        <section className="w-full p-2  ">
-        <header className="p-3  fixed z-10 left-60  border-b right-0">
-          <div className="container flex justify-between items-center">
-            <div>
-             <NavigationHeading />
+        <section className="w-full">
+          <header className="p-3 fixed z-10 left-60 bg-black border-b right-0">
+            <div className="container flex justify-between items-center">
+              <div>
+                <NavigationHeading />
+              </div>
+              <div className="flex gap-6 justify-center">
+                <DropdownMenu>
+                  <DropdownMenuTrigger className="bg-white" asChild>
+                    <Button variant={"outline"} className="bg-white text-white"><PlusIcon className="mr-2"/> Create</Button>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent className="w-40 bg-white" align="end">
+                    <Link href={"/admin/projects/create"}>
+                      <DropdownMenuItem className="hover:text-black text-gray-500 hover:bg-gray-300 cursor-pointer">
+                        Project
+                      </DropdownMenuItem>
+                    </Link>
+                    <Link href={"/admin/blogs/create"}>
+                      <DropdownMenuItem className="hover:text-black text-gray-500 hover:bg-gray-300 cursor-pointer">
+                        Blog
+                      </DropdownMenuItem>
+                    </Link>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <Avatar className="cursor-pointer">
+                      <AvatarImage
+                        src="https://github.com/shadcn.png"
+                        alt="@shadcn"
+                      />
+                      <AvatarFallback>
+                        {user?.email && user.email[0]}
+                      </AvatarFallback>
+                    </Avatar>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent className="w-56 bg-white" align="end">
+                    <DropdownMenuItem className="text-gray-500">
+                      {user.email}
+                    </DropdownMenuItem>
+                    <DropdownMenuSeparator />
+                    <form action={signOut}>
+                      <button className="py-2 px-2 text-sm block hover:bg-gray-300 rounded-md no-underline bg-btn-background w-full text-left">
+                        Logout
+                      </button>
+                    </form>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              </div>
             </div>
-            <div className="flex gap-6 justify-center">
-            <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                  <Button variant={"outline"}><PlusIcon className="mr-2"/> Create</Button>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent className="w-40 bg-white" align="end">
-                  <Link href={"/admin/projects/create"}>
-                    <DropdownMenuItem className="text-gray-500 cursor-pointer">
-                      Project
-                    </DropdownMenuItem>
-                  </Link>
-                  <Link href={"/admin/blogs/create"}>
-                    <DropdownMenuItem className="text-gray-500 cursor-pointer">
-                      Blog
-                    </DropdownMenuItem>
-                  </Link>
-                </DropdownMenuContent>
-              </DropdownMenu>
-            <DropdownMenu >
-      <DropdownMenuTrigger asChild>
-            <Avatar className=" cursor-pointer">
-            <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-            <AvatarFallback>{user?.email && user.email[0]}</AvatarFallback>
-          </Avatar>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-56 z-10 bg-white"  align="end">
-      <DropdownMenuItem className=" cursor-pointer">
-         {user.email}
-        </DropdownMenuItem>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem className=" cursor-pointer">
-          Log out
-          <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+          </header>
+          <div className="absolute left-60 p-9 right-0 bottom-0 top-24 right-5">
+            <div className="container">
+              {children}
             </div>
           </div>
-            </header>
-         {children}
+          {/* <div className="p-5 mt-20 max-w-6xl ml-auto mr-auto w-1/2"></div> */}
         </section>
-          </main>
+      </main>
         )
   // return user ? (
   //   <div className="flex items-center gap-4">
