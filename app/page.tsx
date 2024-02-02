07b88a536a9464d0b9c6b152b958aad5be85c288
neuronarchitecture27 <troisiemeoeilagency@gmail.com>
2024-02-02 02:58:23 +0100

    adding home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,12 @@
+import {LampDemo} from '@/components/HeaderHome'
 
-import { createClient } from "@/utils/supabase/server";
-import { cookies } from "next/headers";
 
 export default async function Index() {
-  const cookieStore = cookies();
 
-  const canInitSupabaseClient = () => {
-    // This function is just for the interactive tutorial.
-    // Feel free to remove it once you have Supabase connected.
-    try {
-      createClient(cookieStore);
-      return true;
-    } catch (e) {
-      return false;
-    }
-  };
-
-  const isSupabaseConnected = canInitSupabaseClient();
 
   return (
-    <div>
-      <h1>Hello Mom</h1>
+    <div className=''>
+      <LampDemo />
     </div>
   );
 }
